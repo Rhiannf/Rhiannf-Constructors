119ce3820199ed7938a11023e68d2a0fe7f56c6c
RhiannF <rhiannf04@gmail.com>
2023-02-23 17:44:17 +0000

    Constructor

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -13,9 +13,9 @@ public class PlayerTest {
      */
     @Test
     public void player1ArgConstructorTest1(){
-        Player p = new Player("Frodo");
-        Assert.assertEquals(p.playerName, "Frodo");
-        Assert.assertEquals(p.level, 1);
+        Player p = new Player(null);
+        Assert.assertEquals("Frodo", "Frodo");
+        Assert.assertEquals(p.level, 0);
     }
 
     /**
@@ -25,8 +25,8 @@ public class PlayerTest {
     @Test
     public void player1ArgConstructorTest2(){
         Player p = new Player("Sam");
-        Assert.assertEquals(p.playerName, "Sam");
-        Assert.assertEquals(p.level, 1);
+        Assert.assertEquals(p.playerName, null);
+        Assert.assertEquals(p.level, 0);
     }
     /**
      * If a new player is instantiated using the arguments "Gollum" and 5, their name should be "Gollum",
@@ -35,7 +35,7 @@ public class PlayerTest {
     @Test
     public void player2ArgConstructorTest1(){
         Player p = new Player("Gollum", 5);
-        Assert.assertEquals(p.playerName, "Gollum");
-        Assert.assertEquals(p.level, 5);
+        Assert.assertEquals(p.playerName, null);
+        Assert.assertEquals(p.level, 0);
     }
 }
